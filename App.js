687d31061fb4033824b8d872519abc22fd8d5216
KJ <cozykoji18@gmail.com>
2020-01-12 18:08:36 +0900

    様々なコンポーネント実装

--- App.js
+++ App.js
@@ -1,13 +1,44 @@
 import React, { Component } from 'react'
-import { View, Image, Button } from 'react-native'
+import { View, Image, Button, TouchableOpacity, ActivityIndicator, FlatList, Text, TextInput } from 'react-native'
 
 export default class App extends Component {
+  constructor() {
+    super()
+    this.state = { text: '' }
+  }
+  showWarning() {
+    console.warn('pressed!!!')
+  }
   render() {
+    const data = [
+      {str:'GuidovanRossum',key:'Guido'},
+      {str:'MatsumotoYukihiro',key:'Matz'},
+      {str:'RasmusLerdorf',key:'Rasmus'}
+    ]
     return (
-      <View style={{flex:1, justifyContent: 'center', alignItems: 'center' }}>
-        <Image style={{width: 64, height: 64 }} 
-               source={require('./assets/sample.png')} />
-        <Button onPress={() => console.warn('pressed')} title={'Press'} />
+      <View style={ {
+          paddingTop: 50,
+          flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center',
+          backgroundColor: '#F5FCFF'
+        } }>
+        <TextInput
+          style={ {
+            width: '100%',
+            borderBottomWidth: 1,
+            borderBottomColor: '#ccc'
+          } }
+        <FlatList 
+          data={data}
+          renderItem={({item}) => { return <Text key={item.key}>{item.str}</Text> } }
+        />
+        <ActivityIndicator />
+        <TouchableOpacity onPress={() => this.showWarning() }>
+          <Image style={{width: 64, height: 64 }} 
+                 source={require('./assets/sample.png')} />
+        </TouchableOpacity>
+        <Button onPress={() => this.showWarning() } title={'Press'} />
       </View>
     )
   }
